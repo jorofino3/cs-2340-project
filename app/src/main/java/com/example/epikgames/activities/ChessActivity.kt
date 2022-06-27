70b87d5248f1645151ee4dab3ca976fe71d84fc6
joeyorofino <josephorofino23@gmail.com>
2022-06-27 12:56:28 -0400

    added button with no functionality

--- app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
+++ app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
@@ -1,7 +1,9 @@
 package com.example.epikgames.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.epikgames.R
 
 class ChessActivity : AppCompatActivity() {
@@ -9,4 +11,11 @@ class ChessActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chess)
     }
+
+    // TODO
+//    val rulesButton = findViewById<ImageButton>(R.id.chessRulesButton)
+//    rulesButton.setOnClickListener {
+//        val intent = Intent(this, WordleRulesActivity::class.java)
+//        startActivity(intent)
+//    }
 }
